b309bcebc1ae4c841ced063792955f4574640663
Rahul <rahulpoonia229@gmail.com>
2024-09-29 19:59:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://jobportal-yt-pi.vercel.app'],
+      origin: '*', // Allows all origins
     credentials: true
 }
 
